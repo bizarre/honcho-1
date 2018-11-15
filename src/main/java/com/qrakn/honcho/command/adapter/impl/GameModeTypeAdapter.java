5e9cac12d013b216ca1d5b4723371ef5452d34c9
Joel Evans <joel@minexd.com>
2018-11-14 22:33:12 -0500

    Add tab completion, Fix empty permission not letting player perform command

--- src/main/java/com/qrakn/honcho/command/adapter/impl/GameModeTypeAdapter.java
+++ src/main/java/com/qrakn/honcho/command/adapter/impl/GameModeTypeAdapter.java
@@ -1,7 +1,9 @@
 package com.qrakn.honcho.command.adapter.impl;
 
 import com.qrakn.honcho.command.adapter.CommandTypeAdapter;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import org.bukkit.GameMode;
 
@@ -22,6 +24,23 @@ public class GameModeTypeAdapter implements CommandTypeAdapter {
     @Override
     public <T> T convert(String string, Class<T> type) {
         return type.cast(MAP.get(string.toLowerCase()));
+    }
+
+    @Override
+    public <T> List<String> tabComplete(String string, Class<T> type) {
+        if (string.isEmpty()) {
+            return new ArrayList<>(MAP.keySet());
+        } else {
+            List<String> completed = new ArrayList<>();
+
+            for (String key : MAP.keySet()) {
+                if (key.toLowerCase().startsWith(string)) {
+                    completed.add(key);
+                }
+            }
+
+            return completed;
+        }
     }
 
 }
--- src/main/java/com/qrakn/honcho/command/adapter/impl/PlayerTypeAdapter.java
+++ src/main/java/com/qrakn/honcho/command/adapter/impl/PlayerTypeAdapter.java
@@ -1,11 +1,29 @@
 package com.qrakn.honcho.command.adapter.impl;
 
 import com.qrakn.honcho.command.adapter.CommandTypeAdapter;
+import java.util.ArrayList;
+import java.util.List;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 public class PlayerTypeAdapter implements CommandTypeAdapter {
+
     @Override
     public <T> T convert(String string, Class<T> type) {
         return type.cast(Bukkit.getPlayer(string));
     }
+
+    @Override
+    public <T> List<String> tabComplete(String string, Class<T> type) {
+        List<String> completed = new ArrayList<>();
+
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            if (player.getName().toLowerCase().startsWith(string.toLowerCase())) {
+                completed.add(player.getName());
+            }
+        }
+
+        return completed;
+    }
+
 }
